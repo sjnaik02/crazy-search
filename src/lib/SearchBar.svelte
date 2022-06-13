b2948318768dab6d949734ca335546e97a0ee3b6
Shourya Naik <sjnaik02@gmail.com>
2022-06-13 23:40:32 +0530

    uses assets in public

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -52,9 +52,9 @@
 
 
 <div class="wrapper">
-  <img src={`src/static/${searchEngine ? searchEngine : defaultSearchEngine}.svg`} alt="search term icon" class="icon">
+  <img src={`/${searchEngine ? searchEngine : defaultSearchEngine}.svg`} alt="search term icon" class="icon">
   <input bind:value={search} on:keydown={handleKeyDown}>
-  <button on:click={handleClick}><img src="src/static/angles-right.svg" alt="Right arrows"></button>
+  <button on:click={handleClick}><img src="/angles-right.svg" alt="Right arrows"></button>
 </div>
 <span>
 {#if autocomplete(search)&&searchEngine === ""}
