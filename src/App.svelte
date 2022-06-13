701b09e6a77c31a8d8740d0c18289e7e599db89a
Shourya Naik <sjnaik02@gmail.com>
2022-06-13 12:16:22 +0530

    ✨ initialised app and removed boilerplate

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  
+</script>
+
+<main>
+  <h1>Hello World</h1>
+  <p>This is a simple Svelte app.</p>
+</main>
+
+<style>
+  
+</style>
---
+++ src/lib/SearchBar.svelte
@@ -0,0 +1,15 @@
+<script>
+  
+</script>
+
+<div class="search-box">
+
+</div>
+
+<style>
+  .search-box{
+    border: 1px solid #000;
+    padding: 10px;
+    margin: 10px;
+  }
+</style>
